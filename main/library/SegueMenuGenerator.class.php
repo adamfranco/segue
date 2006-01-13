6e5a3cf6009acccfb7cebc622055cadbd5e8b2d1
adamfranco <adamfranco>
2006-01-13 20:54:32 +0000

    Adding test action.

--- main/library/SegueMenuGenerator.class.php
+++ main/library/SegueMenuGenerator.class.php
@@ -5,7 +5,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: SegueMenuGenerator.class.php,v 1.3 2006/01/13 19:51:17 adamfranco Exp $
+ * @version $Id: SegueMenuGenerator.class.php,v 1.4 2006/01/13 20:54:32 adamfranco Exp $
  */
 
 /**
@@ -19,7 +19,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: SegueMenuGenerator.class.php,v 1.3 2006/01/13 19:51:17 adamfranco Exp $
+ * @version $Id: SegueMenuGenerator.class.php,v 1.4 2006/01/13 20:54:32 adamfranco Exp $
  */
 
 class SegueMenuGenerator {
@@ -47,57 +47,10 @@ class SegueMenuGenerator {
 
 
 		$mainMenu_item2 =& new MenuItemLink(
-			_("Collections"),
-			$harmoni->request->quickURL("collections", "namebrowse"), 
-			($module == "collections")?TRUE:FALSE,1);
+			_("Plugin Tests"),
+			$harmoni->request->quickURL("plugin_manager", "test"), 
+			($module == "plugin_manager")?TRUE:FALSE,1);
 		$mainMenu->add($mainMenu_item2, "100%", null, LEFT, CENTER);
-		
-		// Collection browse links.
-		// Just show if we are not in a particular collection.
-		if (ereg("collection(s)?|asset", $module)) {			
-			// Collection root
-			if (ereg("^(collection|asset)$", $module)) {
-				// Name browse
-				$mainMenu_item5 =& new MenuItemLink(
-						_("Collection"),
-						$harmoni->request->quickURL("collection", "browse",
-							array('collection_id' => $harmoni->request->get('collection_id'))),
-						($module == "collection")?TRUE:FALSE, 2
-				);
-				$mainMenu->add($mainMenu_item5, "100%", null, LEFT, CENTER);
-			}
-		}
-		
-		$mainMenu_item6 =& new MenuItemLink(
-			_("Exhibitions"),
-			$harmoni->request->quickURL("exhibitions", "browse"), 
-			(ereg("^exhibition.*",$module))?TRUE:FALSE,1);
-		$mainMenu->add($mainMenu_item6, "100%", null, LEFT, CENTER);
-		
-		foreach (array_keys($_SESSION) as $key) {
-			if (ereg("^add_slideshow_wizard_(.+)", $key, $matches)) {
-				$exhibitionAssetId = $matches[1];
-	
-				$item =& new MenuItemLink(
-						_("SlideShow"),
-						$harmoni->request->quickURL("exhibitions", "add_slideshow", 
-							array("exhibition_id" => $exhibitionAssetId)), 
-						($module == "exhibitions" && $action == "add_slideshow" && RequestContext::value("exhibition_id") == $exhibitionAssetId)?TRUE:FALSE, 2
-				);
-				$mainMenu->add($item, "100%", null, LEFT, CENTER);
-			}
-		}
-		
-		$mainMenu_item8 =& new MenuItemLink(
-			_("User Tools"),
-			$harmoni->request->quickURL("user", "main"), 
-			(ereg("^user$", $module))?TRUE:FALSE, 1);
-		$mainMenu->add($mainMenu_item8, "100%", null, LEFT, CENTER);
-		
-		$mainMenu_item7 =& new MenuItemLink(_("Admin Tools"),
-			$harmoni->request->quickURL("admin", "main"), 
-			(ereg("^admin$",$module))?TRUE:FALSE, 1);
-		$mainMenu->add($mainMenu_item7, "100%", null, LEFT, CENTER);
 	
 	
 		return $mainMenu;
---
+++ main/modules/plugin_manager/test.act.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package segue.modules.home
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: test.act.php,v 1.1 2006/01/13 20:54:33 adamfranco Exp $
+ */ 
+
+require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
+
+/**
+ * 
+ * 
+ * @package segue.modules.home
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: test.act.php,v 1.1 2006/01/13 20:54:33 adamfranco Exp $
+ */
+class testAction 
+	extends MainWindowAction
+{
+	/**
+	 * Check Authorizations
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function isAuthorizedToExecute () {
+		return TRUE;
+	}
+	
+	/**
+	 * Return the heading text for this action, or an empty string.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function getHeadingText () {
+		return _("Plugin Tests");
+	}
+	
+	/**
+	 * Build the content for this action
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function buildContent () {
+		$actionRows =& $this->getActionRows();
+		ob_start();
+		
+		
+		$actionRows->add(
+			new Block(ob_get_clean(), STANDARD_BLOCK), 
+			"100%", 
+			null, 
+			CENTER, 
+			CENTER);
+	}	
+}
+
+?>
