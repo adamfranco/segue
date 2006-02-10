771ef58866a9164938218852107e8972580b6f87
adamfranco <adamfranco>
2006-02-10 21:27:43 +0000

    no current need for the prefix.

--- config/id_default.conf.php
+++ config/id_default.conf.php
@@ -10,12 +10,12 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: id_default.conf.php,v 1.2 2006/01/13 18:51:17 adamfranco Exp $
+ * @version $Id: id_default.conf.php,v 1.3 2006/02/10 21:27:43 adamfranco Exp $
  */
  
  	$configuration =& new ConfigurationProperties;
 	$configuration->addProperty('database_index', $dbID);
 	$configuration->addProperty('database_name', $dbName);
-	$configuration->addProperty('id_prefix', $arg0 = 'dev_id-');
-	unset($arg0);
+// 	$configuration->addProperty('id_prefix', $arg0 = 'dev_id-');
+// 	unset($arg0);
 	Services::startManagerAsService("IdManager", $context, $configuration);
