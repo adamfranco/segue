9cf837ad5bac0bbba70eb90aee340eb16565aa0d
adamfranco <adamfranco>
2006-03-28 18:34:20 +0000

    Changing enabledness so the first option is selected.

--- main/library/Display/NavigationNodeRenderer.class.php
+++ main/library/Display/NavigationNodeRenderer.class.php
@@ -6,7 +6,7 @@
  * @copyright Copyright &copy; 2006, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: NavigationNodeRenderer.class.php,v 1.30 2006/03/13 16:30:36 adamfranco Exp $
+ * @version $Id: NavigationNodeRenderer.class.php,v 1.31 2006/03/28 18:34:20 adamfranco Exp $
  */
  
 require_once(HARMONI."GUIManager/Components/MenuItemLinkWithAdditionalHtml.class.php");
@@ -21,7 +21,7 @@ require_once(HARMONI."GUIManager/Components/MenuItemLinkWithAdditionalHtml.class
  * @copyright Copyright &copy; 2006, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: NavigationNodeRenderer.class.php,v 1.30 2006/03/13 16:30:36 adamfranco Exp $
+ * @version $Id: NavigationNodeRenderer.class.php,v 1.31 2006/03/28 18:34:20 adamfranco Exp $
  */
 class NavigationNodeRenderer
 	extends NodeRenderer
@@ -527,7 +527,7 @@ class NavigationNodeRenderer
 			print " disabled='disabled'";
 		}
 		print ">";
-		print "\n\t\t\t\t\t<option value='' disabled='disabled'>"._("Add a new child element...")."</option>";
+		print "\n\t\t\t\t\t<option value=''>"._("Add a new child element...")."</option>";
 		print "\n\t\t\t\t\t<option value='' disabled='disabled'>------------</option>";
 		print "\n\t\t\t\t\t<option value='single_nav'>"._("Container")."</option>";
 		print "\n\t\t\t\t\t<option value='nav'>"._("Navigational Container")."</option>";
