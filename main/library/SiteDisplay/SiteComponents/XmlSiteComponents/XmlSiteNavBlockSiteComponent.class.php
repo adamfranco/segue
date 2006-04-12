d3a4a0128df302467c6032780b370a2c8e7e1057
cws-midd <cws-midd>
2006-04-12 15:22:17 +0000

    sitenav no target

--- main/library/SiteDisplay/SiteComponents/XmlSiteComponents/XmlSiteNavBlockSiteComponent.class.php
+++ main/library/SiteDisplay/SiteComponents/XmlSiteComponents/XmlSiteNavBlockSiteComponent.class.php
@@ -6,7 +6,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: XmlSiteNavBlockSiteComponent.class.php,v 1.1 2006/04/05 16:11:30 adamfranco Exp $
+ * @version $Id: XmlSiteNavBlockSiteComponent.class.php,v 1.2 2006/04/12 15:22:17 cws-midd Exp $
  */ 
 
 /**
@@ -18,12 +18,25 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: XmlSiteNavBlockSiteComponent.class.php,v 1.1 2006/04/05 16:11:30 adamfranco Exp $
+ * @version $Id: XmlSiteNavBlockSiteComponent.class.php,v 1.2 2006/04/12 15:22:17 cws-midd Exp $
  */
 class XmlSiteNavBlockSiteComponent
 	extends XmlNavBlockSiteComponent
 	// implements SiteNavBlockSiteComponent
 {	
+
+	/**
+	 * Answers nothing because this is a top level nav
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 4/12/06
+	 */
+	function getTargetId () {
+		// don't ask me for this... my destination is hard-coded.
+		throwError( new Error("SiteNavBlocks do not have self-defined target_id's", "SiteComponents"));
+	}
+
 	/**
 	 * Accepts a visitor.
 	 * 
