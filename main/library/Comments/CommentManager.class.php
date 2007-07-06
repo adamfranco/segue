1854e332d76efe7a66bc531e0698694ec8455cef
adamfranco <adamfranco>
2007-07-06 17:51:39 +0000

    Fleshing out the Comments package.

---
+++ main/library/Comments/CommentManager.class.php
@@ -0,0 +1,449 @@
+<?php
+/**
+ * @since 6/7/07
+ * @package segue.comments
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: CommentManager.class.php,v 1.1 2007/07/06 17:51:39 adamfranco Exp $
+ */ 
+
+require_once(dirname(__FILE__)."/CommentNode.class.php");
+
+if (!defined('ASC'))
+	define('ASC', 'ASC');
+	
+if (!defined('DESC'))
+	define('DESC', 'DESC');
+
+
+/**
+ * The CommentManager is responsible for loading, accessing, and creating comments.
+ * The CommentManager is a singleton.
+ * 
+ * @since 6/7/07
+ * @package segue.comments
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: CommentManager.class.php,v 1.1 2007/07/06 17:51:39 adamfranco Exp $
+ */
+class CommentManager {
+		
+	/**
+	 * Get the instance of the object.
+	 * This class implements the Singleton pattern. There is only ever
+	 * one instance of the object and it is accessed only via the 
+	 * CommentManager::instance() method.
+	 * 
+	 * @return object Harmoni
+	 * @access public
+	 * @since 5/26/05
+	 * @static
+	 */
+	function &instance () {
+		if (!defined("COMMENT_MANAGER_INSTANTIATED")) {
+			$GLOBALS['__commentManager'] =& new CommentManager();
+			define("COMMENT_MANAGER_INSTANTIATED", true);
+		}
+		
+		return $GLOBALS['__commentManager'];
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function CommentManager () {
+		$this->mediaFileType =& new Type ('segue', 'edu.middlebury', 'media_file',
+				'A file that is uploaded to Segue.');
+	}
+	
+	/**
+	 * Create a new root comment attached to an asset
+	 * 
+	 * @param object Asset $asset The asset this thread is attached to.
+	 * @param object Type $type The type of plugin to use.
+	 * @return object CommentNode
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &createRootComment ( &$assetOrId, &$type ) {
+		if (method_exists($assetOrId, 'getId')) {
+			$asset =& $assetOrId;
+			$id =& $asset->getId();
+		} else {
+			$repositoryManager =& Services::getService("Repository");
+			$idManager =& Services::getService("Id");
+			$repository =& $repositoryManager->getRepository(
+				$idManager->getId("edu.middlebury.segue.sites_repository"));
+			$asset =& $repository->getAsset($assetOrId);
+			$id =& $assetOrId;
+		}
+		
+		$repository =& $asset->getRepository();
+		$commentAsset =& $repository->createAsset("", "", $type);
+		$asset->addAsset($commentAsset->getId());
+		$comment =& $this->getComment($commentAsset);
+		
+		// Clear our order caches
+		$this->_rootComments[$id->getIdString()];
+		$this->_allComments[$id->getIdString()];
+		
+		return $comment;
+	}
+	
+	/**
+	 * Answer a comment object identified by Id or asset
+	 * 
+	 * @param mixed $assetOrId
+	 * @return object CommentNode
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getComment ( &$assetOrId ) {
+		if (method_exists($assetOrId, 'getId')) {
+			$asset =& $assetOrId;
+			$id =& $asset->getId();
+		} else {
+			$id =& $assetOrId;
+		}
+		
+		// Cache the comment if needed
+		if (!isset($this->_comments[$id->getIdString()])) {
+			// Get the assset if we were passed an Id only
+			if (!isset($asset)) {
+				$repositoryManager =& Services::getService("Repository");
+				$idManager =& Services::getService("Id");
+				$repository =& $repositoryManager->getRepository(
+					$idManager->getId("edu.middlebury.segue.sites_repository"));
+				$asset =& $repository->getAsset($id);
+			}
+			
+			$this->_comments[$id->getIdString()] =& new CommentNode($asset);
+		}
+		return $this->_comments[$id->getIdString()];
+	}
+	
+	/**
+	 * Answer the top-level comments attached to an asset
+	 * 
+	 * @param object $assetOrId
+	 * @param string $order The constant ASC or DESC for ascending time (oldest 
+	 *			first) or decending time (recent first).
+	 * @return object Iterator
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getRootComments ( &$assetOrId, $order = ASC ) {
+		if (method_exists($assetOrId, 'getId')) {
+			$asset =& $assetOrId;
+		} else {
+			$repositoryManager =& Services::getService("Repository");
+			$idManager =& Services::getService("Id");
+			$repository =& $repositoryManager->getRepository(
+				$idManager->getId("edu.middlebury.segue.sites_repository"));
+			$asset =& $repository->getAsset($assetOrId);
+		}
+		
+		// Load the replies, their creation times into arrays for caching and 
+		// easy sorting.
+		$assetId =& $asset->getId();
+		$assetIdString = $assetId->getIdString();
+		if (!isset($this->_rootComments[$assetIdString])) {
+			$this->_rootComments[$assetIdString] = array();
+			$this->_rootComments[$assetIdString]['ids'] = array();
+			$this->_rootComments[$assetIdString]['times'] = array();
+				
+			$children =& $asset->getAssets();
+			
+			while ($children->hasNext()) {
+				$child =& $children->next();
+				if (!$this->mediaFileType->isEqual($child->getAssetType())) {
+					$comment =& $this->getComment($child);
+					$dateTime =& $comment->getCreationDate();
+					$this->_rootComments[$assetIdString]['ids'][] =& $comment->getId();
+					$this->_rootComments[$assetIdString]['times'][] = $dateTime->asString();
+				}
+			}
+		}
+		
+		// Sort the comment Ids based on time.
+		array_multisort($this->_rootComments[$assetIdString]['ids'], $this->_rootComments[$assetIdString]['times'], 
+			(($order == ASC)?SORT_ASC:SORT_DESC));
+		
+		$null = null;
+		$comments = new HarmoniIterator($null);
+		foreach ($this->_rootComments[$assetIdString]['ids'] as $id)
+			$comments->add($this->getComment($id));
+		
+		return $comments;
+	}
+	
+	/**
+	 * Answer all of the comments attached to an asset
+	 * 
+	 * @param object $assetOrId
+	 * @param string $order The constant ASC or DESC for ascending time (oldest 
+	 *			first) or decending time (recent first).
+	 * @return object Iterator
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getAllComments ( &$assetOrId, $order = ASC ) {
+		if (method_exists($assetOrId, 'getId')) {
+			$asset =& $assetOrId;
+		} else {
+			$repositoryManager =& Services::getService("Repository");
+			$idManager =& Services::getService("Id");
+			$repository =& $repositoryManager->getRepository(
+				$idManager->getId("edu.middlebury.segue.sites_repository"));
+			$asset =& $repository->getAsset($assetOrId);
+		}
+		
+		// Load the replies, their creation times into arrays for caching and 
+		// easy sorting.
+		$assetId =& $asset->getId();
+		$assetIdString =& $assetId->getIdString();
+		if (!isset($this->_allComments[$assetIdString])) {
+			$this->_allComments[$assetIdString] = array();
+			$this->_allComments[$assetIdString]['ids'] = array();
+			$this->_allComments[$assetIdString]['times'] = array();
+			
+			$rootComments =& $this->getRootComments($asset);
+			$allComments =& new MultiIteratorIterator();
+			while ($rootComments->hasNext()) {
+				$allComments->addIterator(
+					$this->_getDescendentComments($rootComments->hasNext()));
+			}
+			
+			while ($allComments->hasNext()) {
+				$comment =& $allComments->next();
+				$dateTime =& $comment->getCreationDate();
+				$this->_allComments[$assetIdString]['ids'][] =& $comment->getId();
+				$this->_allComments[$assetIdString]['times'][] = $dateTime->asString();
+			}
+		}
+		
+		// Sort the comment Ids based on time.
+		array_multisort($this->_allComments[$assetIdString]['ids'], $this->_allComments[$assetIdString]['times'], 
+			(($order == ASC)?SORT_ASC:SORT_DESC));
+		
+		$comments = new HarmoniIterator(null);
+		foreach ($this->_allComments[$assetIdString]['ids'] as $idString)
+			$comments->add($this->getComment($id));
+		
+		return $comments;
+	}
+	
+	/**
+	 * Answer all comments below and including a comment
+	 * 
+	 * @param object Comment $comment
+	 * @return object Iterator
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &_getDescendentComments ( &$comment ) {
+		$thisComment = array();
+		$thisComment[] =& $comment;
+		
+		$decendents =& new MultiIteratorIterator(new HarmoniIterator($thisComment));
+		$children =& $comment->getReplies();			
+		while ($children->hasNext()) {
+			$child =& $children->next();
+			$decendents->addIterator($this->_getDescendentComments($child));
+		}
+		
+		return $decendents;
+	}
+	
+	/**
+	 * Answer the interface markup needed to display the comments attached to the
+	 * given asset.
+	 * 
+	 * @param object Asset $asset
+	 * @return string
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function getMarkup ( &$asset ) {
+		if (RequestContext::value('order'))
+			$this->setDisplayOrder(RequestContext::value('order'));
+		
+		if (RequestContext::value('displayMode'))
+			$this->setDisplayMode(RequestContext::value('displayMode'));
+		
+		if (RequestContext::value('create_new_comment')) {
+			$comment =& $this->createRootComment(Type::fromString(RequestContext::value('new_comment_type')));
+			$comment->enableEditForm();
+		}
+		$this->addHead();
+		
+		ob_start();
+		
+		// print the ordering form
+		print "\n\n<form action='#' method='post'>";
+		
+		print "\n\t<div style='float: left;'>";
+		print "\n\t<input type='button' name='".RequestContext::name('create_new_comment')."' value='"._('Create New')."'";
+		print " onclick='this.form.action = this.form.action.replace(/#.*/, \"#current_comment\"); this.form.submit();'";
+		print "/>";
+		
+		print "\n\t\t<select name='".RequestContext::name('new_comment_type')."'/>";
+		$pluginManager =& Services::getService('PluginManager');
+		$plugins = $pluginManager->getEnabledPlugins();
+		foreach ($plugins as $key => $pType) {
+			print "\n\t\t\t<option value='".$key."'>".$pType->getKeyword()."</option>";
+		}
+		print "\n\t\t</select> ";
+		print _('Comment');
+		print "\n\t</div>";
+		
+		print "\n\t<div style='float: right; text-align: right;'>";
+		
+		print "\n\t\t<select name='".RequestContext::name('displayMode')."'/>";
+		print "\n\t\t\t<option value='threaded'".(($this->getDisplayMode() == 'threaded')?" selected='selected'":"").">";
+		print _("Threaded")."</option>";
+		print "\n\t\t\t<option value='flat'".(($this->getDisplayMode() == 'flat')?" selected='selected'":"").">";
+		print _("Flat")."</option>";
+		print "\n\t\t</select>";
+		
+		print "\n\t\t<select name='".RequestContext::name('order')."'/>";
+		print "\n\t\t\t<option value='".ASC."'".(($this->getDisplayOrder() == ASC)?" selected='selected'":"").">";
+		print _("Oldest First")."</option>";
+		print "\n\t\t\t<option value='".DESC."'".(($this->getDisplayOrder() == DESC)?" selected='selected'":"").">";
+		print _("Newest First")."</option>";
+		print "\n\t\t</select>";
+		
+		print "\n\t<input type='submit' value='"._("Change")."'/>";
+		
+		print "\n\t</div>";
+		
+		print "\n\t<div style='clear: both;'> &nbsp; </div>";
+		
+		print "\n</form>";
+		
+		// Print out the Comments
+		print "\n<div id='".RequestContext::name('comments')."'>";
+		if ($this->getDisplayMode() == 'flat') {
+			$comments =& $this->getAllComments($asset, $this->getDisplayOrder());
+		} else {
+			$comments =& $this->getRootComments($asset, $this->getDisplayOrder());
+		}
+		
+		while ($comments->hasNext()) {
+			$comment =& $comments->next();
+			print $comment->getMarkup(($this->getDisplayMode() == 'threaded')?true:false);
+		}
+		
+		print "\n</div>";
+		
+		return ob_get_clean();
+	}
+	
+	/**
+	 * Set the display mode
+	 * 
+	 * @param string $mode
+	 * @return void
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function setDisplayMode ($mode) {
+		if ($mode == 'flat' || $mode == 'threaded') {
+			$_SESSION['comment_display_mode'] = $mode;
+		}
+	}
+	
+	/**
+	 * Answer the display mode
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function getDisplayMode () {
+		if (isset($_SESSION['comment_display_mode'])) {
+			return $_SESSION['comment_display_mode'];
+		} else {
+			return 'threaded';
+		}
+	}
+	
+	/**
+	 * Set the ordering of comments
+	 * 
+	 * @param string $mode
+	 * @return void
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function setDisplayOrder ($order) {
+		if ($order == ASC || $order == DESC) {
+			$_SESSION['comment_display_order'] = $order;
+		}
+	}
+	
+	/**
+	 * Answer the display order
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function getDisplayOrder () {
+		if (isset($_SESSION['comment_display_order'])) {
+			return $_SESSION['comment_display_order'];
+		} else {
+			return ASC;
+		}
+	}
+	
+	/**
+	 * Add head Styles
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function addHead () {
+		$harmoni =& Harmoni::instance();
+		$outputHandler =& $harmoni->getOutputHandler();
+		ob_start();
+		print $outputHandler->getHead();
+		
+		print <<< END
+		
+		<style type='text/css'>
+			img.reply_icon {
+				float: left;
+				clear: left;
+			}
+			
+			.comment_title {
+				font-weight: bold;
+				border-top: 1px solid;
+				margin-top: 5px;
+			}
+			
+			.comment_replies {
+				padding-left: 10px;
+			}
+			
+			.comment_reply {
+			 	margin-left: 15px;
+			}
+		</style>
+		
+END;
+		$outputHandler->setHead(ob_get_clean());
+	}
+}
+
+?>
---
+++ main/library/Comments/CommentNode.class.php
@@ -0,0 +1,282 @@
+<?php
+/**
+ * @since 6/7/07
+ * @package segue.comments
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: CommentNode.class.php,v 1.1 2007/07/06 17:51:39 adamfranco Exp $
+ */ 
+
+/**
+ * A CommentNode is an asset that may contain comments. The root of a comment-hierarchy
+ * is a CommentNode, but not a Comment itself. Comments extend CommentNodes. CommentNode
+ * provides access to authorization and settings for Commenting as well as child comments.
+ * 
+ * @since 6/7/07
+ * @package segue.comments
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: CommentNode.class.php,v 1.1 2007/07/06 17:51:39 adamfranco Exp $
+ */
+class CommentNode {
+		
+	/**
+	 * Constructor
+	 * 
+	 * @param object Asset $asset
+	 * @return void
+	 * @access public
+	 * @since 6/7/07
+	 */
+	function CommentNode ( &$asset ) {
+		$this->_asset =& $asset;
+		$this->_enableEditForm = false;
+	}
+	
+	/**
+	 * Answer the Id.
+	 * 
+	 * @return object Id
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getId () {
+		return $this->_asset->getId();
+	}
+	
+	/**
+	 * Answer the id string
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function getIdString () {
+		$id =& $this->getId();
+		return $id->getIdString();
+	}
+	
+	/**
+	 * Answer the date this comment was posted
+	 * 
+	 * @return object DateAndTime
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getCreationDate () {
+		return $this->_asset->getCreationDate();
+	}
+	
+	/**
+	 * Answer the date that the comment was modified
+	 * 
+	 * @return object DateAndTime
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getModificationDate () {
+		return $this->_asset->getModificationDate();
+	}
+	
+	/**
+	 * Answer the subject of the comment.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function getSubject () {
+		return $this->_asset->getDisplayName();
+	}
+	
+	/**
+	 * Answer the comment body.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function getBody () {
+		// Only return a body if we are authorized to view the comment
+		$azManager =& Services::getService("AuthZ");
+		$idManager =& Services::getService("Id");
+		if ($azManager->isUserAuthorized(
+			$idManager->getId("edu.middlebury.authorization.view_comments"),
+			$this->getId()))
+		{
+			$pluginManager =& Services::getService('PluginManager');
+			$plugin =& $pluginManager->getPlugin($this->_asset);
+			
+			// We've just checked our view permission, so use true
+			$plugin->setCanViewFunction('$plugin', 'return true;');
+			
+			// If we are authorized to comment, are the comment author, and there are
+			// no replies yet, allow us to edit the comment
+			if ($azManager->isUserAuthorized(
+					$idManager->getId("edu.middlebury.authorization.comment"),
+					$this->getId())
+				&& $this->isAuthor()
+				&& $this->numReplies() === 0)
+			{
+				$plugin->setCanModifyFunction('$plugin', 'return true;');
+				return $plugin->executeAndGetMarkup(true);
+			} else {
+				$plugin->setCanModifyFunction('$plugin', 'return false;');
+				return $plugin->executeAndGetMarkup(false);
+			}
+		} else {
+			return _("You are not authorized to view this comment.");
+		}
+	}
+	
+	/**
+	 * Answer the number of replies to this comment
+	 * 
+	 * @return integer
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function numReplies () {
+		$replies =& $this->getReplies();
+		return $replies->count();
+	}
+	
+	/**
+	 * Answer the replies in ascending or descending time.
+	 * 
+	 * @param string $order The constant ASC or DESC for ascending time (oldest 
+	 *			first) or decending time (recent first).
+	 * @return iterator
+	 * @access public
+	 * @since 7/3/07
+	 */
+	function &getReplies ( $order = ASC ) {
+		// Load the replies, their creation times into arrays for caching and 
+		// easy sorting.
+		if (!isset($this->_replies)) {
+			$this->_replyIds = array();
+			$this->_replyTimes = array();
+			
+			$mediaFileType =& new Type ('segue', 'edu.middlebury', 'media_file',
+				'A file that is uploaded to Segue.');
+				
+			$children =& $this->_asset->getAssets();
+			
+			while ($children->hasNext()) {
+				$child =& $children->next();
+				if (!$mediaFileType->isEqual($child->getAssetType())) {
+					$dateTime =& $child->getCreationDate();
+					$this->_replyIds[] =& $child->getId();
+					$this->_replyTimes[] = $dateTime->asString();
+				}
+			}
+		}
+		
+		// Sort the reply Ids based on time.
+		array_multisort($this->_replyIds, $this->_replyTimes, 
+			(($order == ASC)?SORT_ASC:SORT_DESC));
+		
+		$null = null;
+		$replies = new HarmoniIterator($null);
+		$commentManager =& CommentManager::instance();
+		foreach ($this->_replyIds as $id) {
+			$replies->add($commentManager->getComment($id));
+		}
+		
+		return $replies;
+	}
+	
+	/**
+	 * Flag the edit form to be displayed
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function enableEditForm () {
+		$this->_enableEditForm = true;
+	}
+	
+	/**
+	 * Answer the Agent that represents the author of the comment.
+	 * 
+	 * @return object Agent
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function &getAuthor () {
+		
+	}
+	
+	/**
+	 * Answer true if the current user is the author of the comment
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function isAuthor () {
+		$author =& $this->getAuthor();
+		$authorId =& $author->getId();
+		$authN =& Services::getService("AuthN");
+		$agentM =& Services::getService("Agent");
+		$authTypes =& $authN->getAuthenticationTypes();
+		while ($authTypes->hasNext()) {
+			$authType =& $authTypes->next();
+			if ($authorId->isEqual($authN->getUserId($authType))) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Answer the markup for this comment.
+	 * 
+	 * @param boolean $showThreadedReplies
+	 * @return string
+	 * @access public
+	 * @since 7/5/07
+	 */
+	function getMarkup ($showThreadedReplies) {
+		ob_start();
+		print "\n\t<div class='comment' id='".$this->getIdString()."'>";
+		
+		print "\n\t\t<div class='comment_display'>";
+		print "\n\t\t\t<div class='comment_title'>";
+		print $this->getSubject();
+		print "\n\t\t\t</div>";
+		
+		print "\n\t\t\t<div class='comment_body'>";
+		print $this->getBody();
+		print "\n\t\t\t</div>";
+		print "\n\t\t</div>";
+		
+				
+		if ($showThreadedReplies) {
+			print "\n\t\t<div class='comment_replies'>";
+			
+			$replies =& $this->getReplies(ASC);
+			while ($replies->hasNext()) {
+				$reply =& $replies->next();
+				print "\n\t\t\t\t<img src='".MYPATH."/icons/reply_indent.png' class='reply_icon'/>";
+				print "\n\t\t\t<div class='comment_reply'>";
+				print $reply->getMarkup(true);
+				
+				print "\n\t\t\t</div>";
+			}
+			
+			print "\n\t\t</div>";
+		}
+		
+		print "\n\t</div>";
+		return ob_get_clean();
+	}
+}
+
+?>
