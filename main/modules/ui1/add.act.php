8618c3ed00ceb499d601c3500035b36783685a7b
adamfranco <adamfranco>
2007-08-23 20:04:44 +0000

    Slots now are attached and detached from the site when adding or deleting the site.

---
+++ main/modules/ui1/add.act.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package segue.modules.site
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: add.act.php,v 1.1 2007/08/23 20:04:44 adamfranco Exp $
+ */ 
+
+require_once(MYDIR."/main/modules/ui2/add.act.php");
+
+?>
