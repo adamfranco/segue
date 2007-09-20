92901e580f0d2ec8670affada97645a80e8f983c
adamfranco <adamfranco>
2007-09-20 17:14:14 +0000

    Libraries: Moved default location of harmoni and polyphony to segue/main/.

--- config/libraries_default.conf.php
+++ config/libraries_default.conf.php
@@ -10,19 +10,19 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: libraries_default.conf.php,v 1.1 2007/07/30 20:20:52 adamfranco Exp $
+ * @version $Id: libraries_default.conf.php,v 1.2 2007/09/20 17:14:14 adamfranco Exp $
  */
 
 /*********************************************************
  * Harmoni Location
  * 		the location on the file system
  *********************************************************/
-define("HARMONI_DIR", MYDIR."/harmoni/");
+define("HARMONI_DIR", MYDIR."/main/harmoni/");
 
 /*********************************************************
  * Polyphony location
  *		DIR: the location on the file system
  *		PATH: the location as seen by the browser. For image urls.
  *********************************************************/
-define("POLYPHONY_DIR", MYDIR."/polyphony/");
-define("POLYPHONY_PATH", dirname(MYURL)."/polyphony/");
+define("POLYPHONY_DIR", MYDIR."/main/polyphony/");
+define("POLYPHONY_PATH", dirname(MYURL)."/main/polyphony/");
