827b2fe4c1609cfdfdd427105b77509d84dacba4
adamfranco <adamfranco>
2007-11-08 20:17:10 +0000

    UI2 editing actions now properly check authorizations before acting.

---
+++ main/library/SiteDisplay/Rendering/IsAuthorizableVisitor.class.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @since 5/24/07
+ * @package segue.libraries.site_display
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: IsAuthorizableVisitor.class.php,v 1.1 2007/11/08 20:17:10 adamfranco Exp $
+ */ 
+
+require_once(dirname(__FILE__)."/SiteVisitor.interface.php");
+
+/**
+ * Return true if passed to site component that can have authorizations applied
+ * 
+ * @since 5/24/07
+ * @package segue.libraries.site_display
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: IsAuthorizableVisitor.class.php,v 1.1 2007/11/08 20:17:10 adamfranco Exp $
+ */
+class IsAuthorizableVisitor 
+	implements SiteVisitor
+{
+	
+	/**
+	 * Visit a Block
+	 * 
+	 * @param object BlockSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitBlock ( BlockSiteComponent $siteComponent ) {
+		return true;
+	}
+	
+	/**
+	 * Visit a Block
+	 * 
+	 * @param object BlockSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitBlockInMenu ( BlockSiteComponent $siteComponent ) {
+		return true;
+	}
+	
+	/**
+	 * Visit a Navigation Block
+	 * 
+	 * @param object NavBlockSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitNavBlock ( NavBlockSiteComponent $siteComponent ) {
+		return true;
+	}
+	
+	/**
+	 * Visit a Site Navigation Block
+	 * 
+	 * @param object SiteNavBlockSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitSiteNavBlock ( SiteNavBlockSiteComponent $siteComponent ) {
+		return true;
+	}
+	
+	/**
+	 * Visit a Fixed Organizer
+	 * 
+	 * @param object FixedOrganizerSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitFixedOrganizer ( FixedOrganizerSiteComponent $siteComponent ) {
+		return false;
+	}
+	
+	/**
+	 * Visit a the fixed Organizer of a nav block
+	 * 
+	 * @param object NavOrganizerSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitNavOrganizer ( NavOrganizerSiteComponent $siteComponent ) {
+		return false;
+	}
+	
+	/**
+	 * Visit a Flow/Content Organizer
+	 * 
+	 * @param object FlowOrganizerSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitFlowOrganizer ( FlowOrganizerSiteComponent $siteComponent ) {
+		return false;
+	}
+	
+	/**
+	 * Visit a MenuOrganizerSiteComponent
+	 * 
+	 * @param object MenuOrganizerSiteComponent $siteComponent
+	 * @return mixed
+	 * @access public
+	 * @since 8/31/07
+	 */
+	public function visitMenuOrganizer ( MenuOrganizerSiteComponent $siteComponent ) {
+		return false;
+	}
+}
+
+?>
