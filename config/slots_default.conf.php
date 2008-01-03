0d518a2e71f58619713d68f0af739ba2157d82e9
adamfranco <adamfranco>
2008-01-03 19:45:58 +0000

    *** empty log message ***

--- config/slots_default.conf.php
+++ config/slots_default.conf.php
@@ -10,7 +10,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: slots_default.conf.php,v 1.1 2007/08/22 20:08:50 adamfranco Exp $
+ * @version $Id: slots_default.conf.php,v 1.2 2008/01/03 19:45:58 adamfranco Exp $
  */
  
  	$idMgr = Services::getService("Id");
@@ -19,11 +19,14 @@
 	PersonalSlot::$validGroups[] = $idMgr->getId('CN=All Faculty,OU=General,OU=Groups,DC=middlebury,DC=edu');
 	PersonalSlot::$validGroups[] = $idMgr->getId('CN=All Staff,OU=General,OU=Groups,DC=middlebury,DC=edu');
 	PersonalSlot::$validGroups[] = $idMgr->getId('CN=Students_By_Year,OU=Groups,DC=middlebury,DC=edu');
-	
 	PersonalSlot::$validGroups[] = $idMgr->getId('CN=All LS People,OU=LS Lists,OU=Groups,DC=middlebury,DC=edu');
+	PersonalSlot::$validGroups[] = $idMgr->getId('CN=MIIS Faculty,OU=Groups,DC=middlebury,DC=edu');
+	PersonalSlot::$validGroups[] = $idMgr->getId('CN=MIIS Staff,OU=Groups,DC=middlebury,DC=edu');
 	
 	
 	// Add group ids which correspond to faculty or other instructors
 	SegueCourseSection::$instructorGroups[] = $idMgr->getId('CN=All Faculty,OU=General,OU=Groups,DC=middlebury,DC=edu');
 // 	SegueCourseSection::$instructorGroups[] = $idMgr->getId('CN=All Staff,OU=General,OU=Groups,DC=middlebury,DC=edu');
 	SegueCourseSection::$instructorGroups[] = $idMgr->getId('CN=All LS Faculty,OU=LS Lists,OU=Groups,DC=middlebury,DC=edu');
+	SegueCourseSection::$instructorGroups[] = $idMgr->getId('CN=MIIS Faculty,OU=Groups,DC=middlebury,DC=edu');
+	SegueCourseSection::$instructorGroups[] = $idMgr->getId('CN=MIIS Staff,OU=Groups,DC=middlebury,DC=edu');
