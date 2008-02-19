d471085b434c77be68db7c33e325a98ef6417469
adamfranco <adamfranco>
2008-02-19 16:57:10 +0000

    URLs: POLYPHONY_PATH constant by default is now based on the MYPATH constant rather than the MYURL constant to allow more flexibility in the URL structure.

--- config/libraries_default.conf.php
+++ config/libraries_default.conf.php
@@ -10,7 +10,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: libraries_default.conf.php,v 1.2 2007/09/20 17:14:14 adamfranco Exp $
+ * @version $Id: libraries_default.conf.php,v 1.3 2008/02/19 16:57:10 adamfranco Exp $
  */
 
 /*********************************************************
@@ -22,7 +22,7 @@ define("HARMONI_DIR", MYDIR."/main/harmoni/");
 /*********************************************************
  * Polyphony location
  *		DIR: the location on the file system
- *		PATH: the location as seen by the browser. For image urls.
+ *		PATH: the location as seen by the browser. For image urls and javascript files.
  *********************************************************/
 define("POLYPHONY_DIR", MYDIR."/main/polyphony/");
-define("POLYPHONY_PATH", dirname(MYURL)."/main/polyphony/");
+define("POLYPHONY_PATH", trim(MYPATH, '/')."/main/polyphony/");
