b996f0e6ffa0974253a5b5fb9ff05c69d06d2224
adamfranco <adamfranco>
2008-04-08 16:09:24 +0000

    Updating default database configuration to add Harmoni_Db configuration.

--- config/database_default.conf.php
+++ config/database_default.conf.php
@@ -10,17 +10,51 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: database_default.conf.php,v 1.3 2007/09/04 18:00:42 adamfranco Exp $
+ * @version $Id: database_default.conf.php,v 1.3.2.1 2008/04/08 16:09:24 adamfranco Exp $
  */
  
  	$configuration = new ConfigurationProperties;
 	Services::startManagerAsService("DatabaseManager", $context, $configuration);
 	
-	//Set up the database connection
+/*********************************************************
+ * Set up the database connection
+ *********************************************************/
+
 	$databaseManager = Services::getService("DatabaseManager");
 	$dbName = "my_segue_database";
 	$dbID = $databaseManager->addDatabase( new MySQLDatabase("localhost", $dbName,"test","test") );
 	$databaseManager->pConnect($dbID);
 	
 	define("IMPORTER_CONNECTION", $dbID);
-	unset($databaseManager); // done with that for now
+	
+/*********************************************************
+ * Set up a Harmoni_Db connection if desired.
+ *		http://harmoni.sourceforge.net/wiki/index.php/Harmoni_Db
+ *
+ * Using the Harmoni_Db requires installation of the PDO
+ * extension
+ *		http://www.php.net/pdo
+ * as well as a database-specific PDO driver for your database
+ *		http://www.php.net/manual/en/ref.pdo.php#pdo.drivers
+ * 
+ * Using Harmoni_Db allows the use of prepared statements
+ * and can improve the performance of some page loads by
+ * 30% or better.
+ *********************************************************/
+
+// 	$db = Harmoni_Db::factory('Pdo_Mysql', array(
+// 		'host'     => 'localhost',
+// 		'username' => 'test',
+// 		'password' => 'test',
+// 		'dbname'   => $dbName,
+// 		'adapterNamespace' => 'Harmoni_Db_Adapter'
+// 	));
+// 	
+// 	Harmoni_Db::registerDatabase('segue_db', $db);	
+	
+	
+/*********************************************************
+ * Enable tracing of methods that execute database queries
+ *********************************************************/
+// $databaseManager->recordQueryCallers = true;
+// $db->recordQueryCallers = true;
