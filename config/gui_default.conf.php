4d400038b1a4f6d89bf9608038d1f7400d1646ae
Adam Franco <afranco@middlebury.edu>
2008-05-06 17:44:35 -0400

    Adding first attempt at new theme for Gui2 theming system

--- config/gui_default.conf.php
+++ config/gui_default.conf.php
@@ -13,7 +13,7 @@
  * @version $Id: gui_default.conf.php,v 1.7 2007/09/21 15:49:24 adamfranco Exp $
  */
 
-require_once(dirname(__FILE__)."/../themes/SimpleThemeWhite/SimpleThemeWhite.class.php");
+require_once(HARMONI.'Gui2/GuiManager.class.php');
 
 
 // :: GUIManager setup ::
@@ -22,20 +22,22 @@ require_once(dirname(__FILE__)."/../themes/SimpleThemeWhite/SimpleThemeWhite.cla
 	$configuration = new ConfigurationProperties;
 	$configuration->addProperty('database_index', $dbID);
 	$configuration->addProperty('database_name', $dbName);
-	$configuration->addProperty('default_theme', new SimpleThemeWhite);
+	$configuration->addProperty('default_theme', 'RoundedYellow');
 	$configuration->addProperty('character_set', 'utf-8');
 	$configuration->addProperty('document_type', 'text/html');
 	$configuration->addProperty('document_type_definition', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">');
 	$configuration->addProperty('xmlns', 'http://www.w3.org/1999/xhtml');
 	
+	// Theme sources
+	$configuration->addProperty('sources', array(
+		array(	'type' => 'directory',
+				'path' => MYDIR.'/themes-dist'),
+		array(	'type' => 'directory',
+				'path' => MYDIR.'/themes-local'),
 	
-	$arrayOfThemes[] = array("Generic Theme","GenericTheme");
-	$arrayOfThemes[] = array("Simple Theme","SimpleTheme");
-	$arrayOfThemes[] = array("Simple Lines Theme","SimpleLinesTheme");
-	$arrayOfThemes[] = array("Simple Black Theme","SimpleThemeBlack");
-	$arrayOfThemes[] = array("Simple Black Theme","SimpleThemeWhite");
-
-
-	$configuration->addProperty('array_of_default_themes', $arrayOfThemes);
+	));
 	
-	Services::startManagerAsService("GUIManager", $context, $configuration);
+	$guiMgr = new Harmoni_Gui2_GuiManager;
+	$guiMgr->assignConfiguration($configuration);
+	$guiMgr->assignOsidContext($context);
+	Services::registerObjectAsService("GUIManager", $guiMgr);
