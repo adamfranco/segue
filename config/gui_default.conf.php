0bd4181c842d416e88c240167dc6874da5aa5025
Adam Franco <afranco@middlebury.edu>
2008-05-09 17:22:25 -0400

    Moving RoundedYellow to RoundedCorners. Removing RoundedWhite.
    Now that options are in place, there is no need for the second
    'RoundedWhite' theme.

--- config/gui_default.conf.php
+++ config/gui_default.conf.php
@@ -22,7 +22,7 @@ require_once(HARMONI.'Gui2/GuiManager.class.php');
 	$configuration = new ConfigurationProperties;
 	$configuration->addProperty('database_index', $dbID);
 	$configuration->addProperty('database_name', $dbName);
-	$configuration->addProperty('default_theme', 'RoundedYellow');
+	$configuration->addProperty('default_theme', 'RoundedCorners');
 	$configuration->addProperty('character_set', 'utf-8');
 	$configuration->addProperty('document_type', 'text/html');
 	$configuration->addProperty('document_type_definition', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">');
