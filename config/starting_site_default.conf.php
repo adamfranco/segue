994acbaf2c55c03c90eb9ce4fd31a2c758a26583
Adam Franco <afranco@middlebury.edu>
2008-06-16 15:31:16 -0400

    Configuration: Can now set a particular segue site as the default page when users do not specify any parameters.

---
+++ config/starting_site_default.conf.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ *
+ *
+ *
+ * @since 6/16/08
+ * @package segue.config
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */ 
+
+define('SEGUE_STARTING_SITE', '');
