160da5774df082339270dd08b80fce32597f2b66
Adam Franco <afranco@middlebury.edu>
2008-07-21 15:18:27 -0400

    Wizard/Dataport: File upload temporary directory is now configurable using the POLYPHONY_TMP_DIR constant located in a new uploads.conf.php configuration file.

---
+++ config/uploads_default.conf.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Upload locations configuration file.
+ *
+ * USAGE: Copy this file to uploads.conf.php to set custom values.
+ *
+ * @package segue.config
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: libraries_default.conf.php,v 1.4 2008/02/29 20:04:06 adamfranco Exp $
+ */
+
+/*********************************************************
+ * Uncomment to set a custom temporary directory for 
+ * file uploads.
+ *********************************************************/
+// define("POLYPHONY_TMP_DIR", "/tmp");
