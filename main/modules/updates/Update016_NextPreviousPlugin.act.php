a19f350f40c5985c25a426e446d2fcc2fbbd6c10
Adam Franco <afranco@middlebury.edu>
2008-08-27 13:58:58 -0400

    New Next/Previous links plugin for auto-creating links between pages in a section.

---
+++ main/modules/updates/Update016_NextPreviousPlugin.act.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @since 7/8/08
+ * @package segue.updates
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: Update005_RssAndBreadcrumbs.act.php,v 1.1 2008/03/21 19:18:18 adamfranco Exp $
+ */ 
+
+require_once(dirname(__FILE__)."/Update.abstract.php");
+
+/**
+ * Install the RSS feed display plugin
+ * 
+ * @since 7/8/08
+ * @package segue.updates
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: Update005_RssAndBreadcrumbs.act.php,v 1.1 2008/03/21 19:18:18 adamfranco Exp $
+ */
+class Update016_NextPreviousPluginAction
+	extends Update
+{
+		
+	/**
+	 * Answer the date at which this updator was introduced
+	 * 
+	 * @return object DateAndTime
+	 * @access public
+	 * @since 7/8/08
+	 */
+	function getDateIntroduced () {
+		$date = Date::withYearMonthDay(2008, 8, 27);
+		return $date;
+	}
+		
+	/**
+	 * Answer the title of this update
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/8/08
+	 */
+	function getTitle () {
+		return _("Next/Previous Plugin");
+	}
+	
+	/**
+	 * Answer the title of this update
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 7/8/08
+	 */
+	function getDescription () {
+		return _("This update will install and enable the new Next/Previous plugin.");
+	}
+	
+	/**
+	 * Answer true if this update is in place
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 7/8/08
+	 */
+	function isInPlace () {
+		$pluginMgr = Services::getService("PluginManager");
+		
+		if (!$pluginMgr->isInstalled(new Type ('SeguePlugins', 'edu.middlebury', 'NextPrevious')))
+			return false;
+		if (!$pluginMgr->isEnabled(new Type ('SeguePlugins', 'edu.middlebury', 'NextPrevious')))
+			return false;
+			
+		return true;
+	}
+	
+	/**
+	 * Run the update
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 7/8/08
+	 */
+	function runUpdate () {
+		$pluginMgr = Services::getService("PluginManager");
+		
+		try {
+			$pluginMgr->installPlugin(new Type ('SeguePlugins', 'edu.middlebury', 'NextPrevious'));
+			$pluginMgr->enablePlugin(new Type ('SeguePlugins', 'edu.middlebury', 'NextPrevious'));
+		} catch (Exception $e) {
+			printpre($e->getMessage());
+		}
+		
+		return $this->isInPlace();
+	}
+	
+}
+
+?>
