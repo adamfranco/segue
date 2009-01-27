b825405de1f54dbdd0cb3d737cb7bbf7bd84b53c
Alex Chapin <achapin@middlebury.edu>
2009-01-27 16:03:11 -0500

    Added class for aggregating history participation actions

---
+++ main/modules/participation/Participation_HistoryAction.class.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @since 1/27/09
+ * @package segue.modules.participation
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */ 
+require_once(MYDIR."/main/modules/view/SiteDispatcher.class.php");
+require_once(MYDIR."/main/modules/participation/Participation_Action.interface.php");
+require_once(dirname(__FILE__)."/Participation_ModAction.abstract.php");
+// require_once(MYDIR."/main/library/Comments/CommentManager.class.php");
+ 
+/**
+ * get info about create modification action
+ * 
+ * @since 1/27/09
+ * @package segue.modules.participation
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */
+class Participation_HistoryAction
+	implements Participation_Action
+{
+
+	/**
+	 * Constructor
+	 * 
+	 * @param SiteComponent $node 
+	 * @param SeguePluginVersion $version
+	 * @return object
+	 * @access public
+	 * @since 1/27/09
+	 */
+	public function __construct (SiteComponent $node, $version) {
+		$this->_node = $node;
+		$this->_version = $version;
+	}
+	
+	/**
+	 * @var SiteComponent $node
+	 * @access private
+	 * @since 1/27/09
+	 */
+	private $_node;
+
+	/**
+	 * @var SeguePluginVersion $_version
+	 * @access private
+	 * @since 1/27/09
+	 */
+	private $_version;
+	
+	/**
+	 * get the id of a version action 
+	 * 
+	 * @return string version id
+	 * @access public
+	 * @since 1/27/09
+	 */
+	public function getId () {	
+		return $this->getIdPrefix()."::".$this->_version->getVersionId();
+	}
+
+
+	/**
+	 * get id prefix
+	 * 
+	 * @return string
+	 * @access protected
+	 * @since 1/27/09
+	 */
+	protected function getIdPrefix () {
+		return "version";
+	}
+	
+	/**
+	 * get timestamp of version action
+	 * 
+	 * @return DateTime
+	 * @access public
+	 * @since 1/27/09
+	 */
+	public function getTimeStamp ()  {	
+		return $this->_version->getTimestamp();
+	}
+	
+	/**
+	 * get creator of action
+	 * 
+	 * @return Participation_Participant
+	 * @access public
+	 * @since 1/27/09
+	 */
+	public function getParticipant ()  {		
+		$director = SiteDispatcher::getSiteDirector();		
+		$site = $director->getRootSiteComponent($this->_node);
+		
+		$participant = new Participation_Participant($site, 
+			$this->_version->getAgentId());
+				
+		return $participant;
+	}
+	
+	/**
+	 * get category of action (e.g. create, edit, comment...)
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 1/26/09
+	 */
+	public function getCategory () {
+		
+		return "Editor";
+	
+	}
+	
+	/**
+	 * get description of action (e.g. create, edit, comment...)
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 1/26/09
+	 */
+	public function getDescription ()  {
+		
+		return "an editor of this content.";
+	
+	}
+	
+	/**
+	 * get display name of node that action is applied to
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 1/23/09
+	 */
+	public function getTargetDisplayName ()  {
+	
+		$versionNumber = $this->_version->getNumber();
+		$nodeTitle = $this->_node->getDisplayName();
+		return $nodeTitle." (version: ".$versionNumber.")";
+	}
+
+	/**
+	 * get url of node that action is applied to
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 1/23/09
+	 */
+	public function getTargetUrl () {	
+		return SiteDispatcher::quickURL('versioning','compare_versions', 
+			array('node' => $this->_node->getId(), 'late_rev' => $this->_version->getVersionId()));
+	}
+	
+	
+}
+
+
+
+?>
