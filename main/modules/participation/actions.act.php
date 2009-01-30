850a59a696699f7687d2802bbdeaf71d1e8cf9ca
Alex Chapin <achapin@middlebury.edu>
2009-01-30 17:20:18 -0500

    updated to add support for filtering by roles and paginating results

--- main/modules/participation/actions.act.php
+++ main/modules/participation/actions.act.php
@@ -15,6 +15,7 @@ require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php
 require_once(MYDIR."/main/library/SiteDisplay/Rendering/IsHeaderFooterSiteVisitor.class.php");
 require_once(MYDIR."/main/modules/view/SiteDispatcher.class.php");
 require_once(dirname(__FILE__)."/ParticipationView.class.php");
+require_once(dirname(__FILE__)."/ParticipationResultPrinter.class.php");
 //require_once(MYDIR."harmoni/core/Primitives/Chronology/DateAndTime.class.php")
 
 /**
@@ -77,11 +78,12 @@ class actionsAction
 	 */
 	public function buildContent () {
 		$actionRows = $this->getActionRows();				
-		
-		
+				
 		$node = SiteDispatcher::getCurrentNode();
 		$actionRows = $this->getActionRows();
 		$actionRows->add(new Heading(_("Participation for: ").$node->getDisplayName(), 2));
+
+
 				
 		$actionRows->add ( new Block($this->getActionsList(), STANDARD_BLOCK));		
 	}
@@ -95,58 +97,197 @@ class actionsAction
 	 * @since 1/28/09
 	 */
 	private function getActionsList () {
-		ob_start();
-		
 		$node = SiteDispatcher::getCurrentNode();
-		print "\n<table class='history_list'>";
-		print "\n\t<thead>";
-		print "\n\t\t<tr>";
-		print "\n\t\t\t<th>"._("Time")."</th>";
-		print "\n\t\t\t<th>"._("Contributor")."</th>";
-		print "\n\t\t\t<th>"._("Contribution")."</th>";
-		print "\n\t\t\t<th>"._("Role")."</th>";
-		print "\n\t\t</tr>";
-		print "\n\t</thead>";
-		print "\n\t<tbody>";
+		$view = new Participation_View($node);		
+	
 		
-		//get list of participants in site
-		$view = new Participation_View($node);
-		$actions = $view->getActions();		
-		
-		// build a sort array
-		$direction = SORT_DESC;
-		$sortField = 'timestamp';
-		$sortKeys = array();
-		
-		if ($sortField == 'timestamp') {
-			foreach ($actions as $action) {
-				$sortKeys[] = $action->getTimeStamp()->asString();
-			
+		if (RequestContext::value('sort'))
+			$sort = RequestContext::value('sort');
+		else
+			$sort = 'timestamp';
+
+		if (RequestContext::value('participant'))
+			$participant = RequestContext::value('participant');
+		else
+			$participant = 'all';
+
+		if (RequestContext::value('role'))
+			$role = RequestContext::value('role');
+		else
+			$role = 'all';
+
+
+		if (RequestContext::value('direction')) {
+			if (RequestContext::value('direction') == 'DESC') {
+				$direction = SORT_DESC;
+				$reorder = 'ASC';
+				$reorderFlag = '&#94;';
+			} else {
+				$direction = SORT_ASC;
+				$reorder = 'DESC';
+				$reorderFlag = 'v';
 			}
 		} else {
-			throw new InvalidArguementException("Unknown sort field $sortField");
+			$direction = SORT_DESC;
+			$reorder = 'ASC';
+			$reorderFlag = '&#94;';
 		}
+
+		$participants = $view->getParticipants();
 		
+		ob_start();
+
+		print "<form action='";
+		print SiteDispatcher::quickURL('participation','actions', array('node' => $node->getId(), 'sort' => 'timestamp', 'direction' => 'DESC'));
+		print "' method='post'>";
+		
+		// get a list of all participants
+		print "\n\t<select name='participant'";
+		print " onchange='this.form.submit();'";
+		print ">";
+	
+		print "\n\t\t<option value='all'>"._("All Participants")."</option>";	
+		foreach ($participants as $aParticipant) {
+			print "\n\t\t<option value='".$aParticipant->getId()."'";
+			if (RequestContext::value('participant') == $aParticipant->getId()) print " selected='selected'";
+			print ">";
+			print $aParticipant->getDisplayName();
+			print "</option>";		
+		}
+		print "\n\t</select> ";				
+				
+		//get list of actions
+		print "\n\t<select name='role'";
+		print " onchange='this.form.submit();'";	
+		print ">";	
+		print "\n\t\t<option value='all'>"._("All Roles")."</option>";	
+		
+		$roleValues = array('commenter', 'author', 'editor');
+		
+		foreach ($roleValues as $roleValue) {
+			print "\n\t\t<option value='".$roleValue."'";
+			if (RequestContext::value('role') == $roleValue) print " selected='selected'";
+			print ">";
+			print $roleValue;
+			print "</option>";		
+		}
+		print "\n\t</select>";		
+		print "\n\t</form>";
+		
+		
+		// create an array of reorder urls
+		$sortValues = array('timestamp', 'contributor', 'contribution', 'role');
+		
+		$reorderUrl = array();
+		foreach ($sortValues as $sortValue) {
+			$reorderUrl[$sortValue] = SiteDispatcher::quickURL('participation','actions', array('node' => $node->getId(),
+				'sort' => $sortValue, 'direction' => $reorder, 'participant' => $participant, 'role' => $role));
+		}
+
+		
+		ob_start();
+		print "\n\t<thead>";
+		print "\n\t\t<tr>";		
+		print "\n\t\t\t<th><a href='";
+		print $reorderUrl['timestamp'];
+		print "'>"._("Time")." ".(($sort == 'timestamp')?$reorderFlag:"")."</a></th>";
+		print "\n\t\t\t<th><a href='";
+		print $reorderUrl['contributor'];
+		print "'>"._("Contributor")." ".(($sort == 'contributor')?$reorderFlag:"")."</a></th>";
+		print "\n\t\t\t<th><a href='";
+		print $reorderUrl['contribution'];
+		print "'>"._("Contribution")." ".(($sort == 'contribution')?$reorderFlag:"")."</a></th>";		
+		print "\n\t\t\t<th><a href='";
+		print $reorderUrl['role'];
+		print "'>"._("Role")." ".(($sort == 'role')?$reorderFlag:"")."</a></th>";
+		print "\n\t</thead>";
+		$headRow = ob_get_clean();
+		
+		// if participant specified get their actions
+		if (isset($participant) && $participant != 'all') {		
+			$idMgr = Services::getService('Id');
+			$participantId = $idMgr->getId($participant);
+			$participantView = new Participation_Participant($view, $participantId);
+			$actions = $participantView->getActions();
+			
+		//get list of all actions in site	
+		} else {			
+			$actions = $view->getActions();	
+		}
+				
+		// sort actions by sort key
+		$sortKeys = array();
+		if ($sort == 'timestamp') {
+			foreach ($actions as $action) {
+				$sortKeys[] = $action->getTimeStamp()->asString();			
+			}
+		} else if ($sort == 'contributor') {
+			foreach ($actions as $action) {
+				$sortKeys[] = $action->getParticipant()->getDisplayName();			
+			}
+		} else if ($sort == 'contribution') {
+			foreach ($actions as $action) {
+				$sortKeys[] = $action->getTargetDisplayName();			
+			}
+		} else if ($sort == 'role') {
+			foreach ($actions as $action) {
+				$sortKeys[] = $action->getCategoryDisplayName();			
+			}
+		
+		} else {
+			throw new InvalidArguementException("Unknown sort field $sort");
+		}
 		
 		array_multisort($sortKeys, $direction, array_keys($actions), SORT_ASC, $actions);
 		
-		// print out list of participants
-		foreach ($actions as $action) {
-			print "\n\t\t<tr>";
-			print "\n\t\t\t<td>".$action->getTimeStamp()->format("Y-m-d g:i a")."\n\t\t\t</td>";
-			print "\n\t\t\t<td>".$action->getParticipant()->getDisplayName()."\n\t\t\t</td>";
-			print "\n\t\t\t<td>";
-			print "<a href='".$action->getTargetUrl()."'>";
-			print $action->getTargetDisplayName();
-			print "</a>";
-			print "\n\t\t\t</td>";
-			print "\n\t\t\t<td>".$action->getCategoryDisplayName()."\n\t\t\t</td>";
-			print "\n\t\t</tr>";
-			
+		// if role action specified then filter actions
+		$selectedActions = array();
+		if ($role != 'all') {
+			foreach ($actions as $action) {
+				if ($action->getCategoryId() == $role)
+					$selectedActions[] = $action;			
+			}
+		} else {
+			$selectedActions = $actions;
 		}
 		
-		print "\n\t</tbody>";
-		print "\n</table>";
+		
+		$this->_node = $node;
+		$this->_sortValue = $sortValue;
+		$this->_reorder = $reorder;
+		$this->_participant = $participant;
+		$this->_role = $role;
+		
+		$printer = new ParticipationResultPrinter($selectedActions, $headRow, array($this, 'printAction'));
+		print $printer->getMarkup();
+		return ob_get_clean();
+	}
+	
+	/**
+	 * Print out a row.
+	 * 
+	 * @param Participation_Action $action
+	 * @return string
+	 * @access public
+	 * @since 1/30/09
+	 */
+	public function printAction (Participation_Action $action) {
+		ob_start();
+		print  "\n\t\t\t<td class='participation_row'>".$action->getTimeStamp()->format("Y-m-d g:i a")."\n\t\t\t</td>";
+		print "\n\t\t\t<td class='participation_row'><a href='";
+		$participant = $action->getParticipant()->getId()->getIdString();
+		print SiteDispatcher::quickURL('participation','actions', array('node' => $this->_node->getId(),
+			'sort' => $this->_sortValue, 'direction' => $this->_reorder, 'participant' => $participant, 'role' => $this->_role))."'>";
+		print $action->getParticipant()->getDisplayName();
+		print "</a>";
+		print "\n\t\t\t</td>";
+		print "\n\t\t\t<td class = 'participation_row'>";
+		print "<a href='".$action->getTargetUrl()."'>";
+		print $action->getTargetDisplayName();
+		print "</a>";
+		print "\n\t\t\t</td>";
+		print "\n\t\t\t<td class='participation_row'>".$action->getCategoryDisplayName()."</td>";
+	
 		return ob_get_clean();
 	}
 
