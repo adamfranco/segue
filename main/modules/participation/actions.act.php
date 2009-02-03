b183878f3334fab7cf3bcdd694bd42071a5f7b16
Alex Chapin <achapin@middlebury.edu>
2009-02-02 20:17:12 -0500

    refined print out of participation UI

--- main/modules/participation/actions.act.php
+++ main/modules/participation/actions.act.php
@@ -16,6 +16,8 @@ require_once(MYDIR."/main/library/SiteDisplay/Rendering/IsHeaderFooterSiteVisito
 require_once(MYDIR."/main/modules/view/SiteDispatcher.class.php");
 require_once(dirname(__FILE__)."/ParticipationView.class.php");
 require_once(dirname(__FILE__)."/ParticipationResultPrinter.class.php");
+require_once(dirname(__FILE__)."/ParticipationBreadCrumbsVisitor.class.php");
+//require_once(MYDIR."/main/library/SiteDisplay/Rendering/BreadCrumbsVisitor.class.php");
 //require_once(MYDIR."harmoni/core/Primitives/Chronology/DateAndTime.class.php")
 
 /**
@@ -81,11 +83,27 @@ class actionsAction
 				
 		$node = SiteDispatcher::getCurrentNode();
 		$actionRows = $this->getActionRows();
-		$actionRows->add(new Heading(_("Participation for: ").$node->getDisplayName(), 2));
-
-
+		$breadcrumbs = $node->acceptVisitor(new BreadCrumbsVisitor($node));
+		
+		$actionRows->add(new Heading(_("Participation: ").$breadcrumbs, 2));
+		
+		
+		//$actionRows->add ( new Block($this->getViewOptions(), STANDARD_BLOCK));
 				
 		$actionRows->add ( new Block($this->getActionsList(), STANDARD_BLOCK));		
+	}
+
+
+	/**
+	 * Display a list of participants with summary of their contributions
+	 * 
+	 * @param string $id
+	 * @return string XHTML markup
+	 * @access private
+	 * @since 1/28/09
+	 */
+	private function getViewOptions () {
+
 	}
 	
 	/**
@@ -98,9 +116,8 @@ class actionsAction
 	 */
 	private function getActionsList () {
 		$node = SiteDispatcher::getCurrentNode();
-		$view = new Participation_View($node);		
-	
-		
+		$view = new Participation_View($node);	
+
 		if (RequestContext::value('sort'))
 			$sort = RequestContext::value('sort');
 		else
@@ -115,6 +132,11 @@ class actionsAction
 			$role = RequestContext::value('role');
 		else
 			$role = 'all';
+
+		if (RequestContext::value('display'))
+			$display = RequestContext::value('display');
+		else
+			$display = '20';
 
 
 		if (RequestContext::value('direction')) {
@@ -140,7 +162,9 @@ class actionsAction
 		print "<form action='";
 		print SiteDispatcher::quickURL('participation','actions', array('node' => $node->getId(), 'sort' => 'timestamp', 'direction' => 'DESC'));
 		print "' method='post'>";
-		
+		print "<a href = '";
+		print SiteDispatcher::quickURL('participation','summary', array('node' => $node->getId(), 'sort' => 'name', 'direction' => 'ASC'));
+		print "'>"._("Summary")."</a> | ";		
 		// get a list of all participants
 		print "\n\t<select name='participant'";
 		print " onchange='this.form.submit();'";
@@ -156,7 +180,7 @@ class actionsAction
 		}
 		print "\n\t</select> ";				
 				
-		//get list of actions
+		// get list of actions
 		print "\n\t<select name='role'";
 		print " onchange='this.form.submit();'";	
 		print ">";	
@@ -172,8 +196,36 @@ class actionsAction
 			print "</option>";		
 		}
 		print "\n\t</select>";		
-		print "\n\t</form>";
+
+		// print out number of rows to display
+		print "\n\tDisplay: <select name='display'";
+		print " onchange='this.form.submit();'";	
+		print ">";	
 		
+		$displayValues = array(20, 5, 10, 25, 30);
+		
+		foreach ($displayValues as $displayValue) {
+			print "\n\t\t<option value='".$displayValue."'";
+			if (RequestContext::value('display') == $displayValue) print " selected='selected'";
+			print ">";
+			print $displayValue;
+			print "</option>";		
+		}
+		print "\n\t</select>";		
+
+
+		
+		// print out summary of participant's actions
+		if (isset($participant) && $participant != 'all') {		
+			$idMgr = Services::getService('Id');
+			$participantId = $idMgr->getId($participant);
+			$participantView = new Participation_Participant($view, $participantId);
+			$commenterActions = $participantView->getNumActionsByCategory("commenter");
+			$authorActions = $participantView->getNumActionsByCategory("author");
+			$editorActions = $participantView->getNumActionsByCategory("editor");
+			print " (".$commenterActions."-comments, ".$authorActions."-new, ".$editorActions."-edits)";
+		}
+		print "\n\t</form>";
 		
 		// create an array of reorder urls
 		$sortValues = array('timestamp', 'contributor', 'contribution', 'role');
@@ -258,7 +310,7 @@ class actionsAction
 		$this->_participant = $participant;
 		$this->_role = $role;
 		
-		$printer = new ParticipationResultPrinter($selectedActions, $headRow, array($this, 'printAction'));
+		$printer = new ParticipationResultPrinter($selectedActions, $headRow, $display, array($this, 'printAction'));
 		print $printer->getMarkup();
 		return ob_get_clean();
 	}
@@ -273,8 +325,8 @@ class actionsAction
 	 */
 	public function printAction (Participation_Action $action) {
 		ob_start();
-		print  "\n\t\t\t<td class='participation_row'>".$action->getTimeStamp()->format("Y-m-d g:i a")."\n\t\t\t</td>";
-		print "\n\t\t\t<td class='participation_row'><a href='";
+		print  "\n\t\t\t<td valign='top' class='participation_row' style='white-space: nowrap'>".$action->getTimeStamp()->format("Y-m-d g:i a")."\n\t\t\t</td>";
+		print "\n\t\t\t<td valign='top'  class='participation_row' style='white-space: nowrap'><a href='";
 		$participant = $action->getParticipant()->getId()->getIdString();
 		print SiteDispatcher::quickURL('participation','actions', array('node' => $this->_node->getId(),
 			'sort' => $this->_sortValue, 'direction' => $this->_reorder, 'participant' => $participant, 'role' => $this->_role))."'>";
@@ -282,9 +334,13 @@ class actionsAction
 		print "</a>";
 		print "\n\t\t\t</td>";
 		print "\n\t\t\t<td class = 'participation_row'>";
-		print "<a href='".$action->getTargetUrl()."'>";
-		print $action->getTargetDisplayName();
-		print "</a>";
+		print  $action->getTargetDisplayName();
+// 		
+// 		$node = $action->getNode();
+// 		print $node->acceptVisitor(new ParticipationBreadCrumbsVisitor($action->getNode()));
+// 		if ($action->getCategoryId() == "editor") {
+// 			print " (<a href='".$action->getTargetUrl()."'>Version: ".$action->getVersionNumber()."</a>)";
+// 		}		
 		print "\n\t\t\t</td>";
 		print "\n\t\t\t<td class='participation_row'>".$action->getCategoryDisplayName()."</td>";
 	
