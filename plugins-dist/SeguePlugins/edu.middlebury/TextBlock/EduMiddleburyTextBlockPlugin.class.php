adamfranco/segue

commit 7e4bc6e23dcdc6652ac46f7122bc4bb61ce24750

UI2: Fixed XHTML validation error in edit-mode output.
Fixed ids beginning with numbers and in-line Javascript containing ampersands.

plugins-dist/SeguePlugins/edu.middlebury/TextBlock/EduMiddleburyTextBlockPlugin.class.php
@@ -160,12 +160,35 @@ class EduMiddleburyTextBlockPlugin
  	 */
  	function getMarkup () {
  		ob_start();
+ 		
+ 		print<<<END
+
+ 	<script type="text/javascript">
+ 	// <![CDATA[
+ 	
+	/**
+	 * Test an event for shift and not meta or control keys.
+	 * This is done here to prevent in-line '&&' symbols 
+	 * 
+	 * @param Event event
+	 * @return boolean
+	 * @access public
+	 * @since 2/9/09
+	 */
+	function shiftAndNotMetaOrControl (event) {
+		return (event.shiftKey && !(event.metaKey || event.ctrlKey));
+	}
+	 	
+ 	// ]]>
+ 	</script>
+
+END;
  		 		
  		if ($this->editing && $this->canModify()) {
 			$this->printEditForm();
  		} else if ($this->canView()) {
  			if ($this->shouldShowControls()) {
-				print "\n<div onclick='if (event.shiftKey && !(event.metaKey || event.ctrlKey)) { ".$this->locationSend(array('edit' => 'true'))."}'>";
+				print "\n<div onclick='if (shiftAndNotMetaOrControl(event)) { ".$this->locationSend(array('edit' => 'true'))."}'>";
  			}
  			
  			if ($this->hasContent()) {

main/modules/ui2/Rendering/EditModeSiteVisitor.class.php
@@ -663,7 +663,7 @@ END;
 							."opacity: .70; ";
 		ob_start();
 		print "\n<div class='controls_bar' "
-			."id='".$siteComponent->getId()."__controls_bar' "
+			."id='controls_bar__".$siteComponent->getId()."' "
 			."style='"
 			."color: #000; "
 			."min-width: 150px; "
@@ -779,8 +779,8 @@ END;
 		}
 		
 		print " onclick='";
-		print "if (event.shiftKey && (event.metaKey || event.ctrlKey)) { ";
-		print 		"toggleControls(document.get_element_by_id(\"".$siteComponent->getId()."__controls_bar\")); ";
+		print "if (shiftAndMetaOrControl(event)) { ";
+		print 		"toggleControls(document.get_element_by_id(\"controls_bar__".$siteComponent->getId()."\")); ";
 		print 		"if (event.stopPropagation) { ";
 		print			"event.stopPropagation(); ";
 		print 		"} else if (window.event) { ";
@@ -820,7 +820,7 @@ END;
 
 <script type='text/javascript'>
 /* <![CDATA[ */
-
+	
 	function showControls(mainElement) {
  		mainElement.style.borderColor = mainElement.borderColor;
 		var controls = getDescendentByClassName(mainElement, 'controls_bar');
@@ -849,6 +849,19 @@ END;
 		if (windowRight < rightEdge) {
 			window.scrollBy(rightEdge - windowRight, 0);
 		}
+	}
+	
+	/**
+	 * Test an event for shift and meta-keys.
+	 * This is done here to prevent in-line '&&' symbols 
+	 * 
+	 * @param Event event
+	 * @return boolean
+	 * @access public
+	 * @since 2/9/09
+	 */
+	function shiftAndMetaOrControl (event) {
+		return (event.shiftKey && (event.metaKey || event.ctrlKey));
 	}
 	
 	function hideControls(mainElement) {

commit dc79950ff7e4a3a8ca61e43d5e7bfe84c12e5c75

Plugins: TextBlock plugin now doesn't show the "read more" link always.
This bug was introduced in commit 7e4bc6e23dcdc6652ac46f7122bc4bb61ce24750
when I added new javascript to the getMarkup() function, but not to the
getExtendedMarkup() function.

plugins-dist/SeguePlugins/edu.middlebury/TextBlock/EduMiddleburyTextBlockPlugin.class.php
@@ -161,28 +161,7 @@ class EduMiddleburyTextBlockPlugin
  	function getMarkup () {
  		ob_start();
  		
- 		print<<<END
-
- 	<script type="text/javascript">
- 	// <![CDATA[
- 	
-	/**
-	 * Test an event for shift and not meta or control keys.
-	 * This is done here to prevent in-line '&&' symbols 
-	 * 
-	 * @param Event event
-	 * @return boolean
-	 * @access public
-	 * @since 2/9/09
-	 */
-	function shiftAndNotMetaOrControl (event) {
-		return (event.shiftKey && !(event.metaKey || event.ctrlKey));
-	}
-	 	
- 	// ]]>
- 	</script>
-
-END;
+		print $this->getJS();
  		 		
  		if ($this->editing && $this->canModify()) {
 			$this->printEditForm();
@@ -234,11 +213,13 @@ END;
  	function getExtendedMarkup () {
  		ob_start();
  		
+ 		print $this->getJS();
+ 		
  		if ($this->editing && $this->canModify()) {
 			$this->printEditForm();
  		} else if ($this->canView()) {
  			if ($this->shouldShowControls()) {
-				print "\n<div onclick='if (event.shiftKey && !(event.metaKey || event.ctrlKey)) { ".$this->locationSend(array('edit' => 'true'))." event.stopPropagation();}'>";
+				print "\n<div onclick='if (shiftAndNotMetaOrControl(event)) { ".$this->locationSend(array('edit' => 'true'))." event.stopPropagation();}'>";
  			}
  			if ($this->hasContent()) {
 		 		print "\n".$this->parseWikiText($this->cleanHTML($this->getContent()));
@@ -261,6 +242,38 @@ END;
  		}
  		
  		return ob_get_clean();
+ 	}
+ 	
+ 	/**
+ 	 * answer Javascript needed
+ 	 * 
+ 	 * @return string
+ 	 * @access protected
+ 	 * @since 2/10/09
+ 	 */
+ 	protected function getJS () {
+ 		return <<<END
+
+ 	<script type="text/javascript">
+ 	// <![CDATA[
+ 	
+	/**
+	 * Test an event for shift and not meta or control keys.
+	 * This is done here to prevent in-line '&&' symbols 
+	 * 
+	 * @param Event event
+	 * @return boolean
+	 * @access public
+	 * @since 2/9/09
+	 */
+	function shiftAndNotMetaOrControl (event) {
+		return (event.shiftKey && !(event.metaKey || event.ctrlKey));
+	}
+	 	
+ 	// ]]>
+ 	</script>
+
+END;
  	}
  	
  	/**