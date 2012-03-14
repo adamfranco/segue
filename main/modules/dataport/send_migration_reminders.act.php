d9fcd1b0082281b01ea0da3a2e45c1f9bd602663
Adam Franco <afranco@middlebury.edu>
2012-03-14 10:38:24 -0400

    Added a script for sending reminder emails.

---
+++ main/modules/dataport/send_migration_reminders.act.php
@@ -0,0 +1,301 @@
+<?php
+/**
+ * @package segue.dataport
+ * 
+ * @copyright Copyright &copy; 2012, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ */ 
+
+/**
+ * Send emails to users reminding them to deal with their sites.
+ * 
+ * 
+ * 
+ * @package segue.dataport
+ * 
+ * @copyright Copyright &copy; 2012, Middlebury College
+ */
+class send_migration_remindersAction
+	extends Action
+{
+
+	/**
+	 * Constructor
+	 * 
+	 */
+	public function __construct () {
+		$this->users = array();
+		$this->slotsWithoutAdmins = array();
+	}
+		
+	/**
+	 * Authorization. As existence is not sensitive information, allow anonymous access.
+	 *
+	 * @return boolean
+	 * @access public
+	 * @since 3/26/08
+	 */
+	public function isAuthorizedToExecute () {
+		return true;
+	}
+	
+	/**
+	 * Execute
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 3/26/08
+	 */
+	public function execute () {
+		header("Content-Type: text/plain");
+		
+		while(ob_get_level())
+			ob_end_flush();
+		flush();
+		
+		$this->buildUserSiteList();
+		$this->emailUsers();
+		
+		exit;
+	}
+	
+	/**
+	 * Build a list of the sites for which users are admins
+	 * 
+	 * @return null
+	 */
+	protected function buildUserSiteList () {
+		$slotMgr = SlotManager::instance();
+		$slots = $slotMgr->getAllSlots();
+		
+		$roleMgr = SegueRoleManager::instance();
+		$agentMgr = Services::getService('Agent');
+		$increment = round($slots->count()/100);
+		$i = 0;
+		$numSites = 0;
+		$incomplete = 0;
+		
+		// Get the users who can adminster each slot.
+		while ($slots->hasNext()) {
+			$i++;
+			if ($i % $increment == 0) {
+				print '.';
+				flush();
+			}
+			
+			$slot = $slots->next();
+			if (!$slot->isAlias() && $slot->siteExists()) {
+				$numSites++;
+				$status = $slot->getMigrationStatus();
+				if ($status['type'] == 'incomplete') {
+					$incomplete++;
+					
+					$adminIds = $roleMgr->getAgentsWithExplicitRoleAtLeast(
+						$roleMgr->getRole('admin'),
+						$slot->getSiteId(),
+						true
+					);
+					$numAdmins = 0;
+					foreach ($adminIds as $adminId) {
+						// Skip the special groups of
+						// 	edu.middlebury.institute
+						// 	edu.middlebury.agents.users
+						// 	edu.middlebury.agents.anonymous
+						// etc.
+						if (preg_match('/^edu\.middlebury\./', $adminId->getIdString()))
+							continue;
+						
+						try {
+							$agent = $agentMgr->getAgentOrGroup($adminId);
+							if ($agent->isGroup())
+								$numAdmins = $numAdmins + $this->addSlotForGroup($agent, $slot);
+							else
+								$numAdmins = $numAdmins + $this->addSlotForAgent($agent, $slot);
+						} catch (UnknownIdException $e) {
+						}
+						
+					}
+					if (!$numAdmins)
+						$this->slotsWithoutAdmins[] = $slot->getShortname();
+				}
+			}
+		}
+		print "\n$incomplete slots of $numSites are still marked as incomplete\n";
+		print "\nSlots without Admins:\n";
+		foreach ($this->slotsWithoutAdmins as $slot)
+			print "\t".$slot."\n";
+	}
+	
+	/**
+	 * Email users.
+	 * 
+	 * @return null
+	 */
+	protected function emailUsers () {
+		if (!defined('MIGRATION_REMINDER_EMAIL_TEST_ONLY'))
+			define('MIGRATION_REMINDER_EMAIL_TEST_ONLY', false);
+		if (!defined('MIGRATION_REMINDER_EMAIL_TEST_MAX'))
+			define('MIGRATION_REMINDER_EMAIL_TEST_MAX', 1);
+		if (!defined('MIGRATION_REMINDER_EMAIL_TEST_RECIPIENT'))
+			define('MIGRATION_REMINDER_EMAIL_TEST_RECIPIENT', '');
+				
+		$total = count($this->users);
+		print "\n".$total." users will be sent reminders.\n";
+		$i = 0;
+		foreach ($this->users as $user) {
+			$i++;
+			$headers = $this->getEmailHeaders($user);
+			$subject = $this->getEmailSubject($user);
+			$body = $this->getEmailBody($user);
+			
+			if (MIGRATION_REMINDER_EMAIL_TEST_ONLY)
+				$to = MIGRATION_REMINDER_EMAIL_TEST_RECIPIENT;
+			else
+				$to = $user['email'];
+			
+			print str_pad($i, strlen($total), ' ', STR_PAD_LEFT).' of '.$total.'    '.$to."\n";
+			
+			if (MIGRATION_REMINDER_EMAIL_TEST_ONLY && !MIGRATION_REMINDER_EMAIL_TEST_RECIPIENT) {
+				print "\n---------------------------------------------------\n";
+				print $to."\n\n";
+				print $headers."\n\n";
+				print $subject."\n\n";
+				print $body."\n";
+			} else {
+				mail($to, $subject, $body, $headers);
+			}
+			
+			
+			if (MIGRATION_REMINDER_EMAIL_TEST_ONLY && $i >= MIGRATION_REMINDER_EMAIL_TEST_MAX)
+				break;
+		}
+	}
+	
+	/**
+	 * Answer the message body for a user.
+	 * 
+	 * @param array $user
+	 * @return string
+	 */
+	protected function getEmailBody (array $user) {
+		if (!defined('MIGRATION_REMINDER_EMAIL_MESSAGE'))
+			throw new ConfigurationErrorException('MIGRATION_REMINDER_EMAIL_MESSAGE must be defined.');
+		
+		ob_start();
+		print "\n\t<ul>";
+		foreach ($user['slots'] as $slot) {
+			$viewUrl = rtrim(SiteDispatcher::getBaseUrlForSlot($slot), '/').'/sites/'.$slot->getShortname();
+			print "\n\t\t<li>";
+			print "<a href=\"".$viewUrl."\">".$viewUrl."</a>";
+			print "</li>";
+		}
+		print "\n\t</ul>";
+		
+		$message = str_replace('[[USER]]', $user['name'], MIGRATION_REMINDER_EMAIL_MESSAGE);
+		$message = str_replace('[[SITE_LIST]]', ob_get_clean(), $message);
+		
+		return $message;
+	}
+	
+	/**
+	 * Answer the subject 
+	 * 
+	 * @param array $user
+	 * @return string
+	 */
+	protected function getEmailSubject (array $user) {
+		if (!defined('MIGRATION_REMINDER_EMAIL_SUBJECT'))
+			throw new ConfigurationErrorException('MIGRATION_REMINDER_EMAIL_SUBJECT must be defined.');
+		
+		return str_replace('[[USER]]', $user['name'], MIGRATION_REMINDER_EMAIL_SUBJECT);
+	}
+	
+	/**
+	 * Answer a header string for the email.
+	 * 
+	 * @param array $user
+	 * @return string
+	 */
+	protected function getEmailHeaders (array $user) {
+		if (!defined('MIGRATION_REMINDER_EMAIL_FROM_NAME'))
+			throw new ConfigurationErrorException('MIGRATION_REMINDER_EMAIL_FROM_NAME must be defined.');
+		if (!defined('MIGRATION_REMINDER_EMAIL_FROM_MAIL'))
+			throw new ConfigurationErrorException('MIGRATION_REMINDER_EMAIL_FROM_MAIL must be defined.');
+		
+		// To send HTML mail, the Content-type header must be set
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		
+		// Additional headers
+		$headers .= 'From: ' . MIGRATION_REMINDER_EMAIL_FROM_NAME ." <" . MIGRATION_REMINDER_EMAIL_FROM_MAIL . ">\r\n";
+				
+		return $headers;
+	}
+	
+	/**
+	 * Add a slot to a users array for an agent.
+	 * 
+	 * @param Agent $agent
+	 * @param Slot $slot
+	 * @return int
+	 */
+	protected function addSlotForAgent (Agent $agent, Slot $slot) {
+		$agentIdString = $agent->getId()->getIdString();
+		if (!isset($this->users[$agentIdString])) {
+			$email = $this->getAgentEmail($agent);
+			// Don't include people without email addresses
+			if (empty($email))
+				return 0;
+			
+			$this->users[$agentIdString] = array(
+				'name' => $agent->getDisplayName(),
+				'email' => $email,
+				'slots' => array(),
+			);
+		}
+		$this->users[$agentIdString]['slots'][] = $slot;
+		return 1;
+	}
+	
+	/**
+	 * Add a slot to all users in a group.
+	 * 
+	 * @param Group $group
+	 * @param Slot $slot
+	 * @return int
+	 */
+	protected function addSlotForGroup (Group $group, Slot $slot) {
+		$members = $group->getMembers(true);
+		$i = 0;
+		while ($members->hasNext()) {
+			$i = $i + $this->addSlotForAgent($members->next(), $slot);
+		}
+		return $i;
+	}
+	
+	/**
+	 * Answer a email for an agent
+	 * 
+	 * @param Agent $agent
+	 * @return string
+	 */
+	protected function getAgentEmail (Agent $agent) {
+// 		print_r($agent->getProperties()); exit;
+		$propertiesIterator = $agent->getProperties();
+		while ($propertiesIterator->hasNext()) {
+			$properties = $propertiesIterator->next();
+			
+			if (!is_null($properties->getProperty('EMail')))
+				return $properties->getProperty('EMail');
+				
+			if (!is_null($properties->getProperty('email')))
+				return $properties->getProperty('email');
+				
+			if (!is_null($properties->getProperty('Email')))
+				return $properties->getProperty('Email');
+		}
+		return null;
+	}
+}
+
+?>
